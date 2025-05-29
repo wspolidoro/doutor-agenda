320bfc882f33b657a60cc6d55465d6b17aa3e386
wspolidoro <wspolidoro@gmail.com>
2025-05-29 20:32:22 -0300

    fix: formulario reset quando e fechado

--- src/app/(protected)/doctors/_components/upsert-doctor-form.tsx
+++ src/app/(protected)/doctors/_components/upsert-doctor-form.tsx
@@ -78,6 +78,7 @@ export default function UpsertDoctorForm({
   onSuccess,
 }: UpsertDoctorFormProps) {
   const form = useForm<z.infer<typeof formSchema>>({
+    shouldUnregister: true,
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: doctor?.name ?? "",

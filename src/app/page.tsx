df5deab8fe6b8695c198aa2e243b1e0048dd8209
wspolidoro <wspolidoro@gmail.com>
2025-06-02 22:30:13 -0300

    feat: redirect to dashboard on home page load

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import { Button } from "@/components/ui/button";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return <Button>Bootcamp!</Button>;
+  redirect("/dashboard");
 }
